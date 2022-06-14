8b815a3c1e98150e3b8a5be06571f74d97da1d71
Adrian Gould <Adrian.Gould@nmtafe.wa.edu.au>
2022-06-14 13:06:45 +0800

    Collectors Migration

--- database/migrations/2022_06_10_050743_create_collectors_table.php
+++ database/migrations/2022_06_10_050743_create_collectors_table.php
@@ -14,6 +14,9 @@ return new class extends Migration {
     {
         Schema::create('collectors', function (Blueprint $table) {
             $table->id();
+            $table->string('given_name', 128)->nullable();
+            $table->string('family_name', 192);
+            $table->json('cars')->nullable();
             $table->timestamps();
         });
     }
