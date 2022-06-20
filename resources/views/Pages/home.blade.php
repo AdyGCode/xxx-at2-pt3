5e1453c96e69d01da7115402ab3c86ddeee77768
Adrian Gould <Adrian.Gould@nmtafe.wa.edu.au>
2022-06-20 11:40:46 +0800

    Tweak the 'home' screen.

--- resources/views/Pages/home.blade.php
+++ resources/views/Pages/home.blade.php
@@ -9,27 +9,39 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 bg-white border-b border-gray-200 flex">
+                <div class="p-6 bg-white border-b border-gray-200 flex flex-center gap-8">
 
-                    <div class="bg-stone-50 rounded w-96 mx-auto rounded-2xl shadow-lg border-emerald-800 border border-1
-                    overflow-hidden">
-                        <div class="h-48 p-3 overflow-hidden text-emerald-700 font-black text-10xl text-center">
-                            {{ $cars }}
+                    <div class="bg-stone-50 rounded flex-1 rounded-2xl shadow-lg border-emerald-800 border border-1
+                    overflow-hidden mx-0">
+                        <div class="flex justify-between px-8">
+                            <p>
+                                <i class="fa fa-solid fa-car-side text-10xl text-emerald-500 m-2"></i>
+                            </p>
+                            <div class="h-max p-3 overflow-hidden text-emerald-700 font-black text-6xl">
+                                {{-- TODO: make sure no random number added to the cars here --}}
+                                {{ $cars + rand(1,9999) }}
+                            </div>
                         </div>
                         <div class="bg-emerald-800 text-emerald-50 p-3">
-                            <p class="text-2xl text-center">
+                            <p class="text-xl text-center">
                                 Cars
                             </p>
                         </div>
                     </div>
 
-                    <div class="bg-stone-50 rounded w-96 mx-auto rounded-2xl shadow-lg border-emerald-800 border border-1
+                    <div class="bg-stone-50 rounded flex-1 rounded-2xl shadow-lg border-rose-800 border border-1
                     overflow-hidden">
-                        <div class="h-48 p-3 overflow-hidden text-emerald-700 font-black text-10xl text-center">
-                            {{ $collectors }}
+                        <div class="flex justify-between px-8">
+                            <p>
+                                <i class="fa fa-solid fa-address-book text-10xl text-rose-500 m-2"></i>
+                            </p>
+                            <div class="h-max p-3 overflow-hidden text-rose-700 font-black text-6xl">
+                                {{ $collectors }}
+                            </div>
                         </div>
-                        <div class="bg-emerald-800 text-emerald-50 p-3">
-                            <p class="text-2xl text-center">
+
+                        <div class="bg-rose-800 text-rose-50 p-3">
+                            <p class="text-xl text-center">
                                 Collectors
                             </p>
                         </div>
