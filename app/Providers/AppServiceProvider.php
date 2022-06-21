bd5931e5ebbda02f607c84759c875cca16dcd26c
Adrian Gould <Adrian.Gould@nmtafe.wa.edu.au>
2022-06-21 17:17:31 +0800

    Heroku deployment - force https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,7 +23,18 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        // https://stackoverflow.com/questions/68964382/laravel-assets-routes-and-url-not-working-when-browse-https-after-deploy-in-a
+        /*
+         * Solution 2 : There are another way to allow https by forcing in your AppServiceProvider or Create a new
+         * middleware to implement this. In my code snippet I decide to use AppServiceProvider because easy to implement.
+         * Open app/Providers/AppServiceProvider.php in any text editor and write
+         * $this->app['request']->server->set('HTTPS', true);
+         *
+         * You may extra check that APP in production mode.
+         * if(request()->getRequestUri() != '/health' && $this->app->environment('production')) {
+         *             $this->app['request']->server->set('HTTPS', true);
+         * }
+         */
         $this->app['request']->server->set('HTTPS', true);
     }
 }
