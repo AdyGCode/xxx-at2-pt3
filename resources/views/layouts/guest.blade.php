3da54be61ae7bb8bf431865e0995215dbd189d30
Adrian Gould <Adrian.Gould@nmtafe.wa.edu.au>
2022-06-23 09:53:09 +0800

    Home: Update to respond to smaller screen on stats only

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -16,18 +16,18 @@
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
 </head>
-<body class="font-sans antialiased">
-<div class="min-h-screen bg-gray-100 flex flex-col">
-    @include('layouts.navigation')
+<body class="font-sans antialiased min-h-screen bg-gray-100 flex flex-col">
+{{--<div class="min-h-screen bg-gray-100 flex flex-col">--}}
+@include('layouts.navigation')
 
-    <!-- Page Heading -->
-    @if(isset($header))
-        <header class="bg-white shadow">
-            <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                {{ $header ?? 'Welcome'}}
-            </div>
-        </header>
-    @endif
+<!-- Page Heading -->
+@if(isset($header))
+    <header class="bg-white shadow">
+        <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+            {{ $header ?? 'Welcome'}}
+        </div>
+    </header>
+@endif
 
     <!-- Page Content -->
     <main class="flex-grow">
@@ -61,6 +61,6 @@
 
     @include('layouts.footer')
 
-</div>
+{{--</div>--}}
 </body>
 </html>
