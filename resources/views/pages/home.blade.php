50cbfb90544e07c2168dd03abcb1ba384bec59a6
Adrian Gould <Adrian.Gould@nmtafe.wa.edu.au>
2022-06-23 09:51:22 +0800

    Home: Update to respond to smaller screen on stats only

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -15,7 +15,7 @@
                     overflow-hidden mx-0">
                         <div class="flex justify-between gap-4">
                             <p>
-                                <i class="fa fa-solid fa-car-side text-6xl text-emerald-500 ml-4 pt-6"></i>
+                                <i class="fa fa-solid fa-car-side text-3xl lg:text-6xl text-emerald-500 ml-4 pt-6"></i>
                             </p>
                             <div class="h-max p-3 overflow-hidden text-emerald-700 font-black text-3xl lg:text-6xl ">
                                 {{-- TODO: make sure no random number added to the cars here --}}
@@ -33,7 +33,7 @@
                     overflow-hidden">
                         <div class="flex justify-between gap-4">
                             <p>
-                                <i class="fa fa-solid fa-address-book text-6xl text-rose-500 ml-4 pt-6"></i>
+                                <i class="fa fa-solid fa-address-book text-3xl lg:text-6xl text-rose-500 ml-4 pt-6"></i>
                             </p>
                             <div class="h-max p-3 overflow-hidden text-rose-700 font-black text-3xl lg:text-6xl ">
                                 {{ $collectors }}
@@ -51,7 +51,7 @@
                     overflow-hidden">
                         <div class="flex justify-between gap-4">
                             <p>
-                                <i class="fa fa-solid fa-user text-6xl text-indigo-500 ml-4 pt-6"></i>
+                                <i class="fa fa-solid fa-user text-3xl lg:text-6xl text-indigo-500 ml-4 pt-6"></i>
                             </p>
                             <div class="h-max p-3 overflow-hidden text-indigo-700 font-black text-3xl lg:text-6xl ">
                                 {{ $users }}
