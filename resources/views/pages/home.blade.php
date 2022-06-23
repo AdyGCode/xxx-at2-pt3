d7c70f9a46aa2188e007b260eb7913425067616d
Adrian Gould <Adrian.Gould@nmtafe.wa.edu.au>
2022-06-23 11:28:39 +0800

    Home: Update to respond to smaller screen on stats only

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -12,58 +12,42 @@
                 <div class="p-6 bg-white border-b border-gray-200 flex flex-center gap-8">
 
                     <div class="bg-stone-50 rounded flex-1 rounded-2xl shadow-lg border-emerald-800 border border-1
-                    overflow-hidden mx-0">
-                        <div class="flex justify-between gap-4 -m-4">
-                            <p>
-                                <i class="fa fa-solid fa-car-side text-6xl lg:text-6xl text-emerald-500"></i>
-                            </p>
-                            <div class="justify h-max p-3 overflow-hidden text-emerald-700 font-black text-3xl
-                            lg:text-6xl ">
-                                {{-- TODO: make sure no random number added to the cars here --}}
-                                {{ $cars + rand(1,9999) }}
-                            </div>
-                        </div>
-                        <div class="bg-emerald-800 text-emerald-50 p-3">
-                            <p class="text-xl text-center">
-                                Cars
-                            </p>
-                        </div>
+                    overflow-hidden  sm:text-2xl md:text-3xl lg:text-4xl text-center">
+                        <p class="flex justify-between text-emerald-700 font-black m-2 align-text-top">
+                            <span><i class="fa fa-solid fa-car-side text-emerald-500"></i></span>
+                            <span>
+                             {{-- TODO: make sure no random number added to the cars here --}}
+                                {{ $cars + rand(1,9999) + 123456}}
+                            </span>
+                        </p>
+
+                        <p class="bg-emerald-800 text-emerald-50 p-3">
+                            Cars
+                        </p>
                     </div>
 
                     <div class="bg-stone-50 rounded flex-1 rounded-2xl shadow-lg border-rose-800 border border-1
-                    overflow-hidden">
-                        <div class="flex justify-between gap-4">
-                            <p>
-                                <i class="fa fa-solid fa-address-book text-3xl lg:text-6xl text-rose-500 ml-4 pt-6"></i>
-                            </p>
-                            <div class="h-max p-3 overflow-hidden text-rose-700 font-black text-3xl lg:text-6xl ">
-                                {{ $collectors }}
-                            </div>
-                        </div>
+                                overflow-hidden  sm:text-2xl md:text-3xl lg:text-4xl text-center">
+                        <p class="flex justify-between text-rose-700 font-black m-2 align-text-top">
+                            <span><i class="fa fa-solid fa-address-book text-rose-500"></i></span>
+                            <span>{{ $collectors }}</span>
+                        </p>
 
-                        <div class="bg-rose-800 text-rose-50 p-3">
-                            <p class="text-xl text-center">
-                                Collectors
-                            </p>
-                        </div>
+                        <p class="bg-rose-800 text-rose-50 p-3">
+                            Collectors
+                        </p>
                     </div>
 
                     <div class="bg-stone-50 rounded flex-1 rounded-2xl shadow-lg border-indigo-800 border border-1
-                    overflow-hidden">
-                        <div class="flex justify-between gap-4">
-                            <p>
-                                <i class="fa fa-solid fa-user text-3xl lg:text-6xl text-indigo-500 ml-4 pt-6"></i>
-                            </p>
-                            <div class="h-max p-3 overflow-hidden text-indigo-700 font-black text-3xl lg:text-6xl ">
-                                {{ $users }}
-                            </div>
-                        </div>
+                    overflow-hidden  sm:text-2xl md:text-3xl lg:text-4xl text-center">
+                        <p class="flex justify-between text-indigo-700 font-black m-2 align-text-top">
+                            <span><i class="fa fa-solid fa-user text-indigo-500"></i></span>
+                            <span>{{ $users }}</span>
+                        </p>
 
-                        <div class="bg-indigo-800 text-indigo-50 p-3">
-                            <p class="text-xl text-center">
-                                Users
-                            </p>
-                        </div>
+                        <p class="bg-indigo-800 text-indigo-50 p-3">
+                            Users
+                        </p>
                     </div>
 
 
